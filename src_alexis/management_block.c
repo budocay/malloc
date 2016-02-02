5094a906914c66752822e5fcc1d1647867cf0a88
Guillaume Pirou <guillaume.pirou@epitech.eu>
2016-02-02 11:51:38 +0100

    norm

--- src_alexis/management_block.c
+++ src_alexis/management_block.c
@@ -12,54 +12,54 @@
 #include <stdio.h>
 #include "include/malloc.h"
 
-t_block		*fusion_block(t_block *b)
+t_block*        fusion_block(t_block *b)
 {
-  if (!b)
-    return NULL;
-  if (b->next && b->next->free)
+    if (b == NULL)
+        return (NULL);
+    if (b->next != NULL && b->next->free)
     {
-      b->size += SIZE_ALLOC + b->next->size;
-      b->next = b->next->next;
-      if (b->next)
-	b->next->prev = b;
+        b->size += SIZE_ALLOC + b->next->size;
+        b->next = b->next->next;
+        if (b->next)
+            b->next->prev = b;
     }
-  return (b);
+    return (b);
 }
 
-void    split_block(t_block *bl, size_t size)
+void            split_block(t_block *bl, size_t size)
 {
-  t_block *new;
+    t_block*    new;
 
-  new = bl;
-  new->size = (bl->size - size) - SIZE_ALLOC;
-  new->next = bl->next;
-  new->prev = bl;
-  new->free = 1;
-  new->size = size;
-  bl->next = new;
-  if (new->next)
-    new->next->prev = new;
+    new = bl;
+    new->size = (bl->size - size) - SIZE_ALLOC;
+    new->next = bl->next;
+    new->prev = bl;
+    new->free = 1;
+    new->size = size;
+    bl->next = new;
+    if (new->next)
+        new->next->prev = new;
 }
 
-t_block  *need_space(t_block *last, size_t size)
+t_block*        need_space(t_block *last, size_t size)
 {
-  t_block *block;
-  void    *request;
+    t_block*    block;
+    void*       request;
 
-  block = sbrk(0);
-  request = sbrk(size + SIZE_ALLOC);
-  if (request == (void*)-1)
-    return NULL;
-  block->size = size;
-  block->next = NULL;
-  block->prev = last;
-  if (last)
-    last->next = block;
-  block->free = 0;
-  return block;
+    block = sbrk(0);
+    request = sbrk(size + SIZE_ALLOC);
+    if (request == (void*)-1)
+        return (NULL);
+    block->size = size;
+    block->next = NULL;
+    block->prev = last;
+    if (last)
+        last->next = block;
+    block->free = 0;
+    return (block);
 }
 
-t_block *get_block_ptr(void *ptr)
+t_block*        get_block_ptr(void *ptr)
 {
-  return (t_block*)ptr - 1;
+    return ((ptr != NULL) ? (t_block*)ptr - 1 : NULL);
 }
